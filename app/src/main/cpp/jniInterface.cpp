2d422f58375c8cf05bfb7d5f7f66a2bc0ac2a3c5
iMorning <admin@catcompany.cn>
2021-03-27 21:42:47 +0800

    fix bugs in main.cpp

---
+++ app/src/main/cpp/jniInterface.cpp
@@ -0,0 +1,15 @@
+#include <jni.h>
+#include <string>
+
+#include "src/decoder.cpp"
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_imorning_qmc_1decoder_MainActivity_mainProcess(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    std::vector<std::string> qmc = "";
+    std::string decodePath = "";
+    int result = main_qmc_process(qmc, decodePath);
+    return env->NewStringUTF(hello.c_str());
+}
--- app/src/main/java/com/imorning/qmc_decoder/MainActivity.java
+++ app/src/main/java/com/imorning/qmc_decoder/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
-        System.loadLibrary("native-lib");
+        System.loadLibrary("main");
     }
 
     @Override
@@ -19,12 +19,12 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+
     }
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native String mainProcess();
 }
